02de5942ced4eafadb8617152de4bde769502f25
vinv1n <jusipola@student.oulu.fi>
2019-03-25 15:46:19 +0200

    Fix everything

--- speech.py
+++ speech.py
@@ -7,66 +7,69 @@ from gtts import gTTS
 
 
 class TextToSpeech:
+
     def __init__(self):
         self.age = 69
         self.lang = 'fi'
         self.rate = 150
         self.lines = []
-        self.output_file = ""
-        self.input_file = ""
 
-    def read_file(self):
-        self.lines = []
-        with open(self.input_file, 'r') as f:
-            self.lines = f.readlines()
+    def read_file(self, input_file):
+        lines = None
+        with open(input_file, 'r') as f:
+            lines = f.read()
 
-        self.create_speech()
+        return lines
 
-    def create_speech(self):
-
-        final_string = ''
-        for i in self.lines:
-            final_string += i
+    def create_speech(self, lines):
 
         engine = pyttsx3.init()
         engine.setProperty(name='rate', value=1000)
         engine.setProperty(name='voice', value='fi')
         engine.setProperty('age', 69)
-        engine.say(final_string)
+        engine.say(lines)
         engine.runAndWait()
 
-        # create mp3-file
-        self.create_mp3(final_string)
 
-    def create_mp3(self, final_string):
-        gtt = gTTS(text=final_string, lang='fi')
-        gtt.save(self.input_file + '.mp3')
-        os.system('mpg123' + self.input_file + '.mp3')
+    def create_mp3(self, output_file, lines):
+        gtt = gTTS(text=lines, lang='fi')
+        gtt.save(output_file + '.mp3')
 
-    def run(self, output_file, input_file):
-        self.output_file = output_file
+    def run(self, input_file, output_file="defult", output_type="mp3"):
+
+        if not input_file:
+            sys.exit("No input file")
+
         if not os.path.exists(input_file):
             raise IOError("Invalid path to file")
-        self.input_file = input_file
-        try:
-            self.read_file()
-        except KeyboardInterrupt:
-            sys.exit('No more tomi :DDD\n'
-                     '???\n'
-                     'Profit')
+
+        lines = self.read_file(input_file)
+        if not lines:
+            sys.exit("No input text")
+
+        if output_type == "mp3":
+            self.create_mp3(output_file, lines)
+        elif output_type == "both":
+            self.create_speech(lines=lines)
+            self.create_mp3(output_file, lines)
+        else:
+            self.create_speech(lines)
+
 
 def main():
     parser = argparse.ArgumentParser(prog="speech")
     parser.add_argument('--output', help="Name of output file", required=False)
     parser.add_argument('--input', help="Path to input file")
+    parser.add_argument('--type', help="Path to input file")
     args = vars(parser.parse_args())
 
     input_file = args.get('input')
     output_file = 'default'
     if args.get('output'):
-        output_file = args.get('output')
+        output_file = args.get('output', "")
+
     try:
-        TextToSpeech().run(output_file=output_file, input_file=input_file)
+        TextToSpeech().run(output_file=output_file, input_file=input_file, output_type=args.get("type"))
     except IOError as e:
         print("Error {}".format(e))
 
